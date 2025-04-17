9c1db7dc65084f51facc0e3acf9ec43f71be5f10
Hasnul Sanna <hasnulsanna7@gmail.com>
2025-04-17 20:10:17 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version="0.0.0",
+    author="Hasnul Sanna",
+    author_email="hasnulsanna7@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
+
